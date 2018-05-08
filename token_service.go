592f142dd4886c4ced59c6460b02982888e77567
Iñigo Fuente <fuente.inigo@gmail.com>
2018-05-08 15:59:14 +0200

    correccion

--- token_service.go
+++ token_service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -37,6 +38,8 @@ type TokenService struct {
 func (srv *TokenService) Decode(tokenString string) (*CustomClaims, error) {
 	fmt.Println(tokenString)
 	// Parse the token
+	splitToken := strings.Split(tokenString, "Bearer")
+	tokenString = splitToken[1]
 	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return key, nil
 	})
