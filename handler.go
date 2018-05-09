34dec07618b39d54f9b5c46d987e4b47cb6494b0
Iñigo Fuente <fuente.inigo@gmail.com>
2018-05-09 10:51:50 +0200

    correccion

--- handler.go
+++ handler.go
@@ -132,8 +132,8 @@ func (srv *service) ValidateToken(ctx context.Context, req *pb.Token, res *pb.Re
 		return errors.New("invalid user")
 	}
 
-	res.Token = &pb.Token{Valid: true} 
-	res.User = claims.User
+	res.Token = &pb.Token{Valid: true}
+	res.User = &pb.User{Email: claims.User.Email}
 
 	return nil
 }
